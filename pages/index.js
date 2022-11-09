fef9506e3f8e4e78889eb39b92c91a847097ba27
fymgit <fabio.yugo@gmail.com>
2022-11-09 19:23:29 -0300

    atualização aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
-import Menu from "../src/components/Menu";
+import Menu from "../src/components/Menu/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 
 
@@ -9,6 +10,8 @@ function HomePage() {
     const estiloHomePage = {
         //backgroundColor: "yellow", fontSize: "36px"
     }
+    //const valorDoFiltro = "t"
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
     console.log(config.playlists)
     return (
         <>
@@ -18,9 +21,11 @@ function HomePage() {
                 flexDirection: "column",
                 flex: 1,
             }}>
-                <Menu />
+                {/* Prop Drilling */}
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+                <Banner />
                 <Header />
-                <Timeline lists={config.playlists} />
+                <Timeline searchValue={valorDoFiltro} lists={config.playlists} />
             </div>
         </>
     )
@@ -35,7 +40,24 @@ export default HomePage
 //        </div>
 //    )
 //}
+const StyledBanner = styled.div `
+    background: cyan;
+    background-image: url(""https://images.unsplash.com/photo-1538481199705-c710c4e965fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"")
+    height: 250px;
+    img {
+        height: 250px;
+        object-fit: cover;
+    }
 
+`
+
+function Banner() {
+    return (
+        <StyledBanner>
+            <img src="https://images.unsplash.com/photo-1538481199705-c710c4e965fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1565&q=80"/>
+        </StyledBanner>
+    )    
+}
 
 const StyledHeader = styled.div `
     img {
@@ -44,7 +66,6 @@ const StyledHeader = styled.div `
         border-radius: 50%;
     }
     .user-info {
-        margin-top: 50px;
         display: flex;
         align-items: center;
         width: 100%;
@@ -67,24 +88,28 @@ function Header() {
     )
 }
 
-function Timeline (propriedade) {
+function Timeline ({searchValue, ...propriedades}) {
     //console.log("Dentro do componente", propriedade.lists)
-    const playlistsNames = Object.keys(propriedade.lists)// o lists se refere ao nome dado na props do componente HomePage()
+    const playlistsNames = Object.keys(propriedades.lists)// o lists se refere ao nome dado na props do componente HomePage()
     // statement , o tipo de loop do For e que não é aceito no React
     // Retorno por expressão, no caso do Map() e ForEach() por exemplo 
     // porém o forEach() retorna undefined, pois diferente do map() ele não retorna nada, o map() retorna uma nova array, convertendo os itens da arr em uma "nova coisa"
     return (
        <StyledTimeline>
             {playlistsNames.map(function(playlistName) {
-                const videos = propriedade.lists[playlistName]
+                const videos = propriedades.lists[playlistName]
                 //console.log(videos)
                 return (
-                    <section>
+                    <section key={playlistName}>
                         <h2>{playlistName}</h2>
                         <div>
-                            {videos.map((video) => {
+                            {videos.filter((video) =>{
+                                const titleNormalized = video.title.toLowerCase();
+                                const searchValueNormalized = searchValue.toLowerCase();
+                                return titleNormalized.includes(searchValueNormalized)
+                            }).map((video) => {
                                     return (
-                                        <a href={video.url}>
+                                        <a key={video.url} href={video.url}>
                                             <img src={video.thumb} />
                                             <span>
                                                 {video.title}
